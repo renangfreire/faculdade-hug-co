d8d8d8cf5aa6c78730057955f8a4dff5b6459920
renangfreire <79239135+renangfreire@users.noreply.github.com>
2024-06-11 17:45:19 +0000

    Deploying to gh-pages from @ renangfreire/faculdade-hug-co@028a0963e4b0e8e9a8e937a4d1d5ebfa9aeb6648 🚀

---
+++ index.8fedcd33.js
@@ -0,0 +1,2 @@
+window.addEventListener("scroll",function(){let e=document.querySelector(".navbar");window.scrollY>600?e.classList.add("scrolled"):e.classList.remove("scrolled")});
+//# sourceMappingURL=index.8fedcd33.js.map
