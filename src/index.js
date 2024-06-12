59b6cf1494e8e54bfb6f01fa4230f26aa6ad3271
Renan Freire <renangomesfreire@gmail.com>
2024-06-11 23:49:45 -0300

    feat: change control page to real repo and create real project index

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 // Para animar se soubrar tempo
 // https://animate.style/
 
+// https://scrollrevealjs.org/guide/installation.html
+// https://animejs.com/documentation/#domNode
+
+const users = [
+  {name:"João", age: 25, city: "São Paulo"},
+  {name:"Maria", age: 30, city: "Rio de Janeiro"},
+  {name:"José", age: 35, city: "Curitiba"},
+]
+
 window.addEventListener('scroll', function() {
+    const heroContainer = document.querySelector(".header")
     const navbar = document.querySelector('.navbar');
-    if (window.scrollY > 600) { // Ajuste o valor conforme necessário
+    if (window.scrollY > heroContainer.clientHeight) { // Ajuste o valor conforme necessário
       navbar.classList.add('scrolled');
     } else {
       navbar.classList.remove('scrolled');
     }
 });
+
