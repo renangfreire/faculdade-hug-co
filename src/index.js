f705ae2776a54a0d43c9545560c32ce251f2335b
Renan Freire <renangomesfreire@gmail.com>
2024-06-16 20:26:22 -0300

    feat: create mk-tech page, anime page, interstellar and news page with scss for each

--- src/index.js
+++ src/index.js
@@ -4,19 +4,20 @@
 // https://scrollrevealjs.org/guide/installation.html
 // https://animejs.com/documentation/#domNode
 
-const users = [
-  {name:"João", age: 25, city: "São Paulo"},
-  {name:"Maria", age: 30, city: "Rio de Janeiro"},
-  {name:"José", age: 35, city: "Curitiba"},
-]
-
 window.addEventListener('scroll', function() {
     const heroContainer = document.querySelector(".header")
     const navbar = document.querySelector('.navbar');
-    if (window.scrollY > heroContainer.clientHeight) { // Ajuste o valor conforme necessário
-      navbar.classList.add('scrolled');
-    } else {
+
+    const hasScrolledClass = navbar.classList.contains("scrolled")
+    
+    if (window.scrollY < heroContainer.clientHeight) {
       navbar.classList.remove('scrolled');
+    } else {
+      if(hasScrolledClass){
+        return
+      }
+      
+      navbar.classList.add('scrolled');
     }
 });
 
