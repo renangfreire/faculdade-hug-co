12434c6d65a5d1f05860e43e37ac2cac720087ba
renangfreire <79239135+renangfreire@users.noreply.github.com>
2024-06-16 23:27:22 +0000

    Deploying to gh-pages from @ renangfreire/faculdade-hug-co@f705ae2776a54a0d43c9545560c32ce251f2335b 🚀

---
+++ index.2b38c8a2.js
@@ -0,0 +1,2 @@
+window.addEventListener("scroll",function(){let e=document.querySelector(".header"),l=document.querySelector(".navbar"),s=l.classList.contains("scrolled");if(window.scrollY<e.clientHeight)l.classList.remove("scrolled");else{if(s)return;l.classList.add("scrolled")}});
+//# sourceMappingURL=index.2b38c8a2.js.map
--- index.5998ab16.js
+++
@@ -1,2 +0,0 @@
-window.addEventListener("scroll",function(){let e=document.querySelector(".header"),l=document.querySelector(".navbar");window.scrollY>e.clientHeight?l.classList.add("scrolled"):l.classList.remove("scrolled")});
-//# sourceMappingURL=index.5998ab16.js.map
